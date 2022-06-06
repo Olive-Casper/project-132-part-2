67a83d39b0676722dc618e6770f9147d1b2001fc
Olive-Casper <79584417+Olive-Casper@users.noreply.github.com>
2022-06-06 20:34:27 +0530

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,47 @@
+img = "";
+status1 = "";
+
+function preload()
+{
+    img = loadImage('bed pic.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('coccssd', modelLoaded);
+    document.getElementById("status").innerHTMLL = "Status : Detecting Objects";
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#FF0000");
+    rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult()
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
---
+++ main.js
@@ -0,0 +1,14 @@
+function picture1()
+{
+    window.location = "bed.html";
+}
+
+function picture2()
+{
+    window.location = "class.html";
+}
+
+function picture3()
+{
+    window.location = "desk.html";
+}
